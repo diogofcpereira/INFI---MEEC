f002932b7ec972626c30e0077973a8bc08a2bf4a
Diogo Pereira <144951138+diogofcpereira@users.noreply.github.com>
2025-02-15 14:59:04 +0000

    Add files via upload

---
+++ Shoop_Floor_Simulation/Config/Table.ml
@@ -0,0 +1,120 @@
+§
+Language0§Language1
+Status§Status
+Tapete 1§Tapete 1
+Cin§Cin
+Cin1§Cin1
+ON / OFF§ON / OFF
+AUTOMATIC§AUTOMATIC
+MANUAL§MANUAL
+MAINTENANCE§MAINTENANCE
+Cin2§Cin2
+Cin3§Cin3
+Cin4§Cin4
+AT1§AT1
+AT1_in0§AT1_in0
+AT1_in2§AT1_in2
+AT1_in3§AT1_in3
+AT1_in4§AT1_in4
+AT1_in1§AT1_in1
+AT1_out1§AT1_out1
+AT1_out2§AT1_out2
+AT1_out3§AT1_out3
+AT1_out4§AT1_out4
+AT1_out5§AT1_out5
+AT1_out6§AT1_out6
+AT2§AT2
+AT2_out0§AT2_out0
+AT2_out2§AT2_out2
+AT2_out3§AT2_out3
+AT2_out4§AT2_out4
+AT2_out1§AT2_out1
+AT2_in1§AT2_in1
+AT2_in2§AT2_in2
+AT2_in3§AT2_in3
+AT2_in4§AT2_in4
+AT2_in5§AT2_in5
+AT2_in6§AT2_in6
+Cell0§Cell0
+Cell0_Ctop§Cell0_Ctop
+Cell0_Cbot§Cell0_Cbot
+Cell0_Cmid§Cell0_Cmid
+Cell1§Cell1
+Cell1_Mtop§Cell1_Mtop
+Cell1_Cmid§Cell1_Cmid
+Cell1_Mbot§Cell1_Mbot
+Cell_Cmid§Cell_Cmid
+ON§ON
+OFF§OFF
+Cell2_Cmid§Cell2_Cmid
+Cell3_Cmid§Cell3_Cmid
+Cell4_Cmid§Cell4_Cmid
+Cell5_Cmid§Cell5_Cmid
+Cell6_Cmid§Cell6_Cmid
+Cell_Mbot§Cell_Mbot
+Cell2_Mbot§Cell2_Mbot
+Cell3_Mbot§Cell3_Mbot
+Cell4_Mbot§Cell4_Mbot
+Cell5_Mbot§Cell5_Mbot
+Cell6_Mbot§Cell6_Mbot
+Roll_f§Roll_f
+Roll1f§Roll1f
+Roll2f§Roll2f
+Roll3f§Roll3f
+Roll4f§Roll4f
+Tapete 2§Tapete 2
+Conveyor Cin1§Conveyor Cin1
+Running§Running
+Stop§Stop
+Shoop Floor Simulator§Shoop Floor Simulator
+Alarms§Alarms
+Events§Events
+Active§Active
+Ack§Ack
+Message§Message
+Date§Date
+Time§Time
+Class 1§Class 1
+Class 2§Class 2
+To ack§To ack
+Excluded§Excluded
+Historical view§Historical view
+Alarms history§Alarms history
+Events history§Events history
+Historical View§Historical View
+Start date§Start date
+Start time§Start time
+End date§End date
+End time§End time
+Events History§Events History
+Chart§Chart
+Charts§Charts
+TOOL ACTION§TOOL ACTION
+1§1
+Select Tool:§Select Tool:
+2§2
+3§3
+STOP CONVEYOR§STOP CONVEYOR
+BLOCK MACHINE§BLOCK MACHINE
+Tool:§Tool:
+Conveyor1§Conveyor1
+Conveyor2§Conveyor2
+Conveyor3§Conveyor3
+conveyor1§conveyor1
+conveyor2§conveyor2
+conveyor3§conveyor3
+sensor1§sensor1
+sensor2§sensor2
+Conveyor 1 overloaded!§Conveyor 1 overloaded!
+Conveyor 2 Overload!§Conveyor 2 Overload!
+Conveyor 1 switched on.§Conveyor 1 switched on.
+Conveyor 2 switched on.§Conveyor 2 switched on.
+Conveyor 1 is full!§Conveyor 1 is full!
+Conveyor 2 is full!§Conveyor 2 is full!
+Conveyor4§Conveyor4
+Conveyor5§Conveyor5
+Operating§Operating
+Machine Status:§Machine Status:
+automatico§automatico
+manual§manual
+manutenção§manutenção
